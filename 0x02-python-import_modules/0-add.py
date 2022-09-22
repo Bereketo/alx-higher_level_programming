47c09025937c183464ef3228b1882e637583a514
Bereketo <bereketalebachew87@gmail.com>
2022-09-22 12:22:11 -0400

    import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-a=1
-b=2
-from  add_0 import add
-print("{} + {} = {}".format(a,b,add(a, b)))
+  ''' sum of 1 and 2'''
+  a=1
+  b=2
+  from  add_0 import add
+  print("{} + {} = {}".format(a,b,add(a, b)))
