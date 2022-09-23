d429727fefd20401f83707ab76fa69e448edae25
Bereketo <bereketalebachew87@gmail.com>
2022-09-23 05:58:34 -0400

    import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,6 @@ if __name__ == '__main__':
     from sys import argv
     add = 0
     leng = len(argv)
-    if (leng == 1):
-        print(0)
     for i in range(1, leng):
         add = add + int(argv[i])
     print(add)
