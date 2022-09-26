54a47471969ac5d89e104d6850c1cd3d44929b48
Bereketo <bereketalebachew87@gmail.com>
2022-09-26 09:03:33 -0400

    python-data-structures

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    inlen = len(matrix[0])
+    leng = len(matrix)
+    for i in range(leng):
+        for j in range(inlen):
+            print("{}".format(matrix[i][j]), end=" ")
+        print()
