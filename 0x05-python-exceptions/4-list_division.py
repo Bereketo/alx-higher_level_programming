c04048ab72b07d41f73570b3ef588e36753a7d06
Bereketo <bereketalebachew87@gmail.com>
2022-10-11 06:21:25 -0400

    list_divison

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i]/my_list_2[i]
         except IndexError:
-            print("out of  range")
+            print("out of range")
             result = 0
         except (TypeError, ValueError):
             print("Wrong type")
