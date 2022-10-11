83d474ad5241f4368026e02dfe41e67dfd8e29de
Bereketo <bereketalebachew87@gmail.com>
2022-10-11 05:47:52 -0400

    safe print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     """
     try:
         res = a/b
-    except (ZeroDivisionError,TypeError):
+    except (ZeroDivisionError, TypeError):
         res = None
     finally:
         print("Inside result: {}".format(res))
