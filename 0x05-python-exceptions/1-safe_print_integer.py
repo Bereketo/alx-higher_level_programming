313b1a3c1dbeacdc7f7eadca35d5eeeca68bbf59
Bereketo <bereketalebachew87@gmail.com>
2022-10-11 02:55:01 -0400

    safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,9 +6,6 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
+        return True
     except ValueError:
-        break
-    else:
-       return False
-
-    return  True 
+        return False
