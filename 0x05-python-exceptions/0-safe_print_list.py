869c82aa6bfc181ba8bb164ebf9a3c7f549d8ebc
Bereketo <bereketalebachew87@gmail.com>
2022-10-11 01:29:45 -0400

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
         while(i <= x):
             for m in my_list:
                 i++
-         return i
+        return i
     except IndexError:
          print("index out of range")
