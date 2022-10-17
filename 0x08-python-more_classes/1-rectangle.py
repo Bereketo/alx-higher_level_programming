02d98494818aeee894f2576dd639d7da677246fd
Bereketo <bereketalebachew87@gmail.com>
2022-10-17 18:52:43 -0400

    python more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+""" Defines a rectangle class with setter and getter methods """
+
 
 class Rectangle:
+    """a method to initialize """
     def __init__(self, height, width):
         self.__width = width
         self.__height = height
 
+    """ a method to return the width of rectangle """
     def width(self):
         return self.__width
 
+    """ a method to set the width of rectangle"""
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -16,9 +21,11 @@ class Rectangle:
         else:
             self.__width = value
 
+    """ a method to return the height of rectangle  """
     def height(self):
         return self.__height
 
+    """ a method to set the height of rectangle """
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
