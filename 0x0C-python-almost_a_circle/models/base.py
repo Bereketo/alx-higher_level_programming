3f96eb9fb349e53f397428ffa3b2e2e47301ce8f
Bereketo <bereketalebachew87@gmail.com>
2022-11-04 02:21:54 -0400

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,14 +3,15 @@
 
 
 class Base:
-   """The base class for other classes in this project
-      __nb_objects is a class attribute
-   """
+    """The base class for other classes in this project
+        __nb_objects is a class attribute
+    """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """initializes the base"""
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects + = 1
-            self.id =Base.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
