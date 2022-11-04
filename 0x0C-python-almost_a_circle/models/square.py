f03ff5e6b92a6ced2b304b45038072bb133a0160
Bereketo <bereketalebachew87@gmail.com>
2022-11-04 05:35:05 -0400

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,9 +7,7 @@ class Square(Rectangle):
     """ a square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super.__init__(id, x, y, width, height)
-        self.width = size
-        self.height = size
+        super.__init__(size, size, x, y, id)
 
     def __str__(self):
         """Return the print() and str() representation of the Square"""
