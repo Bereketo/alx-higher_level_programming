12a312203c31f04912e247c25d5a2555058cb672
Bereketo <bereketalebachew87@gmail.com>
2022-11-04 05:31:42 -0400

    python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Square class """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ a square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super.__init__(id, x, y, width, height)
+        self.width = size
+        self.height = size
+
+    def __str__(self):
+        """Return the print() and str() representation of the Square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y,
+                                                 self.size)
