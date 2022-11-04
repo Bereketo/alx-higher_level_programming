e6ba620052353870e3e2045a6a31bad260c0d5da
Bereketo <bereketalebachew87@gmail.com>
2022-11-04 04:57:43 -0400

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,36 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
                                                        self.y, self.width,
                                                        self.height)
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute """
+        if args and len(args) != 0:
+            k = 0
+            for arg in args:
+                if k = 0:
+                    self.id = arg
+                elif k = 1:
+                    self.width = arg
+                elif k = 2:
+                    self.height = arg
+                elif k = 3:
+                    self.x = arg
+                elif k = 4:
+                    self.y = arg
+                k += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
