0bbf3a51d7a5ef8713feb70595e965705bc986ce
Bereketo <bereketalebachew87@gmail.com>
2022-11-04 02:47:14 -0400

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
-        if not isinstance(width, int):
+        if type(width) != int:
             raise TyperError("width must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
@@ -40,7 +40,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
-        if not isinstance(height, int):
+        if type(height) != int:
             raise TypeError("height must be an integer")
         if height <= 0:
             raise ValueError("height must be > 0")
